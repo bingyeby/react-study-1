e8ac66fc2a2f05bb587ae8645e7574d8321caab2
syl <bingyeby@163.com>
2018-04-10 23:41:44 +0800

    't'

--- src/demo-b/ShareholderStrength.js
+++ src/demo-b/ShareholderStrength.js
@@ -4,16 +4,66 @@ import {render} from 'react-dom';
 import {connect} from 'dva'
 import echarts from 'echarts'
 import PropTypes from 'prop-types'
+import _ from 'lodash'
 
 import styles from './ShareholderStrengthStyle.less'
 
 class ShareholderStrength extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            tableData: [
+                [], [], []
+            ],
+            labelDataArr: [
+                {
+                    value: '其他应收款(关联方)',
+                    isActive: true
+                },
+                {
+                    value: '其他应付款(关联方)',
+                    isActive: false
+
+                },
+                {
+                    value: '长期应收款(关联方)',
+                    isActive: false
+                }
+            ]
+        }
     }
+
+    activeHandler = (clickObj) => {
+        let index = 0;
+        _.each(this.state.labelDataArr, function (n, i) {
+            n.isActive = false;
+            if (clickObj.value === n.value) {
+                n.isActive = true;
+                index = i;
+            }
+        });
+        this.setState({
+            labelDataArr: this.state.labelDataArr
+        })
+    };
 
     render() {
         return (<div>
+
+            <div className={styles.labelOuter}>
+                {
+                    this.state.labelDataArr.map((n, i) => {
+                        return (<span key={`labelDataArr${i}`}
+                                      className={n.isActive ? styles.active : styles.normal}
+                                      onClick={this.activeHandler.bind(this, n)}>{n.value}</span>)
+                    })
+                }
+            </div>
+            <div>
+
+            </div>
+
+
             <div className='moduleTitle'>股东实力</div>
             <div className={styles.tableOuter}>
                 <table>
